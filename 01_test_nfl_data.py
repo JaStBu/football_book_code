05b1ae06be6f8b81b5bafe043911445d83d0ee53
JaStBu <108984983+JaStBu@users.noreply.github.com>
2025-03-23 11:54:59 +0100

    messing around with the passing data

---
+++ 01_test_nfl_data.py
@@ -0,0 +1,36 @@
+## import packages
+## make sure you installed both packages using pip in the terminal
+import pandas as pd
+import nfl_data_py as nfl
+import os
+
+cols = nfl.see_pbp_cols()
+print("Columns in the play by play data:")
+for col in cols:
+    print(col)
+
+## Load data from the package
+chap_1_file = "./data/pbp_py_chap_1.csv"
+
+if os.path.isfile(chap_1_file):
+    pbp_py = pd.read_csv(chap_1_file, low_memory=False)
+else:
+    pbp_py = nfl.import_pbp_data([2021])
+    pbp_py.to_csv(chap_1_file)
+
+## filter out passing data
+filter_crit = 'play_type == "pass" & air_yards.notnull()'
+pbp_py_p = (
+    pbp_py.query(filter_crit)
+    .groupby(["passer_id", "passer"])
+    .agg({"air_yards": ["count", "mean", "min", "max"]})
+)
+
+## format and print data for passing plays
+pbp_py_p.columns = list(map("_".join, pbp_py_p.columns.values))
+sort_crit = "air_yards_count > 100"
+print(
+    pbp_py_p.query(sort_crit)
+    .sort_values(by="air_yards_mean", ascending=[False])
+    .to_string()
+)
